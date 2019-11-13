2a1c6bd62d544ec4afebb360211d00c4675e4abe
matija plevnik <matija.plevnik@gmail.com>
2019-11-13 18:02:56 +0100

    Added testing command for javascript codes

--- commands/random/JavaTesting.js
+++ commands/random/JavaTesting.js
@@ -1,1 +1,22 @@
 const {
+    Command
+} = require('discord.js-commando');
+
+sum = (a, b) => {
+    sum1 = (a) => a + 1;
+    return sum1(a) + b;
+}
+module.exports = class HeartCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'jstest',
+            aliases: ['test', 'testjs'],
+            group: 'random',
+            memberName: 'jstest',
+            description: 'Testing out random JavaScript commands/codes.',
+        });
+    }
+    async run(msg) {
+        return msg.channel.send(sum(10, 20));
+    }
+};
